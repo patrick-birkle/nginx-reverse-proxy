70d8b5ee66e29ba40d44931bdd637a2e836aa4f8
Patrick Birkle <company@it-birkle.de>
2020-03-04 18:00:05 +0100

    updated start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 NUM_NETWORK=$(docker network ls --filter name=reverse-proxy-net --format "{{.ID}}" | wc -l)
 
-if [[ ${NUM_NETWORK} -eq 0 ]]; then
+if [ ${NUM_NETWORK} -eq 0 ]; then
   docker network create reverse-proxy-net
 fi
 
 actual_dir=$(pwd)
 
-if [[ ! -e "${actual_dir}/.env" ]]; then
+if [ ! -e "${actual_dir}/.env" ]; then
   touch ${actual_dir}/.env
-  echo "PROXY_PATH=${actual_dir}/data" >> ${actual_dir}/.env
+  echo "PROXY_PATH=${actual_dir}" >> ${actual_dir}/.env
   chmod 600 ${actual_dir}/.env
 fi
 
