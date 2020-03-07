ce0a122e97db3540805a5232131cad190497b4e2
Patrick Birkle <company@it-birkle.de>
2020-03-07 13:27:27 +0100

    updated docker-compose.yaml to not use data folder by env variable

--- start.sh
+++ start.sh
@@ -6,13 +6,4 @@ if [ ${NUM_NETWORK} -eq 0 ]; then
   docker network create reverse-proxy-net
 fi
 
-actual_dir=$(pwd)
-
-if [ -e "${actual_dir}/.env" ]; then
-  rm -f ${actual_dir}/.env
-fi
-
-echo "PROXY_PATH=${actual_dir}" >> ${actual_dir}/.env
-chmod 600 ${actual_dir}/.env
-
 docker-compose up -d
